2c45bbdcc56fab21ee4e22790e53da5d587394d0
AakashJyoti <akjyoti572@gmail.com>
2023-01-21 00:39:39 +0530

    update

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,7 +1,4 @@
 import { useState, useEffect } from "react";
-import API_KEY from "./keys";
-
-const CONTEXT_KEY = "05c0466d56e9387f3";
 
 const useGoogleSearch = (term) => {
   const [data, setData] = useState(null);
@@ -9,7 +6,7 @@ const useGoogleSearch = (term) => {
   useEffect(() => {
     const fetchData = async () => {
       fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${term}`
       )
         .then((response) => response.json())
         .then((result) => {
